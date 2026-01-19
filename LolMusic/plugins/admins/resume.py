117a26b501a7717e83de015b6759db355abb9a0b
HYPER-OP-92 <np564605@gmail.com>
2026-01-19 15:27:43 +0530

    ˹ sʜʀᴇᴇ ꭙ ϻᴜsɪᴄ ˼

---
+++ LolMusic/plugins/admins/resume.py
@@ -0,0 +1,37 @@
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Revange 😎
+
+# 🧑‍💻 Developer : t.me/dmcatelegram
+# 🔗 Source link : https://github.com/hexamusic/LolMusic
+# 📢 Telegram channel : t.me/dmcatelegram
+# =======================================================
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from LolMusic import app
+from LolMusic.core.call import Sona
+from LolMusic.utils.database import is_music_playing, music_on
+from LolMusic.utils.decorators import AdminRightsCheck
+from LolMusic.utils.inline import close_markup
+from config import BANNED_USERS
+
+
+@app.on_message(filters.command(["resume", "cresume"]) & filters.group & ~BANNED_USERS)
+@AdminRightsCheck
+async def resume_com(cli, message: Message, _, chat_id):
+    if await is_music_playing(chat_id):
+        return await message.reply_text(_["admin_3"])
+    await music_on(chat_id)
+    await Sona.resume_stream(chat_id)
+    await message.reply_text(
+        _["admin_4"].format(message.from_user.mention), reply_markup=close_markup(_)
+    )
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Revange 😎
+
+# 🧑‍💻 Developer : t.me/dmcatelegram
+# 🔗 Source link : https://github.com/hexamusic/LolMusic
+# 📢 Telegram channel : t.me/dmcatelegram
+# =======================================================
