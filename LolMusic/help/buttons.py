b4900776f80dd5923a1685d42265dadbfc15857f
HYPER-OP-92 <np564605@gmail.com>
2026-01-20 07:23:34 +0530

    Update buttons.py

--- LolMusic/help/buttons.py
+++ LolMusic/help/buttons.py
@@ -14,28 +14,28 @@ from LolMusic import app
 class BUTTONS(object):
     BBUTTON = [
     [
-        InlineKeyboardButton("• ᴀᴄᴛɪᴏɴ •", callback_data="TOOL_BACK HELP_06"),
-        InlineKeyboardButton("• ᴀɴᴛɪ-ғʟᴏᴏᴅ •", callback_data="TOOL_BACK HELP_11"),
-        InlineKeyboardButton("• ᴀpproval •", callback_data="TOOL_BACK HELP_12"),
+        InlineKeyboardButton("•ᴀᴄᴛɪᴏɴ•", callback_data="TOOL_BACK HELP_06"),
+        InlineKeyboardButton("•ᴀɴᴛɪ-ғʟᴏᴏᴅ•", callback_data="TOOL_BACK HELP_11"),
+        InlineKeyboardButton("•ᴀpproval•", callback_data="TOOL_BACK HELP_12"),
     ],
     [
-        InlineKeyboardButton("• ᴄʜᴀᴛ-ɢᴘᴛ •", callback_data="TOOL_BACK HELP_01"),
-        InlineKeyboardButton("• ɢɪᴛʜᴜʙ •", callback_data="TOOL_BACK HELP_09"),
-        InlineKeyboardButton("• ɢʀᴏᴜᴘ •", callback_data="TOOL_BACK HELP_07"),
+        InlineKeyboardButton("•ᴄʜᴀᴛ-ɢᴘᴛ•", callback_data="TOOL_BACK HELP_01"),
+        InlineKeyboardButton("•ɢɪᴛʜᴜʙ•", callback_data="TOOL_BACK HELP_09"),
+        InlineKeyboardButton("•ɢʀᴏᴜᴘ•", callback_data="TOOL_BACK HELP_07"),
     ],
     [
-        InlineKeyboardButton("• ʜɪsᴛᴏʀʏ •", callback_data="TOOL_BACK HELP_08"),
-        InlineKeyboardButton("• ɪɴғᴏ •", callback_data="TOOL_BACK HELP_03"),
-        InlineKeyboardButton("• ᴘᴜʀɢᴇ •", callback_data="TOOL_BACK HELP_13"),
+        InlineKeyboardButton("•ʜɪsᴛᴏʀʏ•", callback_data="TOOL_BACK HELP_08"),
+        InlineKeyboardButton("•ɪɴғᴏ•", callback_data="TOOL_BACK HELP_03"),
+        InlineKeyboardButton("•ᴘᴜʀɢᴇ•", callback_data="TOOL_BACK HELP_13"),
     ],
     [
-        InlineKeyboardButton("• sᴛɪᴄᴋᴇʀ •", callback_data="TOOL_BACK HELP_05"),
-        InlineKeyboardButton("• ᴛᴀɢ-ᴀʟʟ •", callback_data="TOOL_BACK HELP_04"),
-        InlineKeyboardButton("• ᴛᴏᴏʟs •", callback_data="TOOL_BACK HELP_10"),
+        InlineKeyboardButton("•sᴛɪᴄᴋᴇʀ•", callback_data="TOOL_BACK HELP_05"),
+        InlineKeyboardButton("•ᴛᴀɢ-ᴀʟʟ•", callback_data="TOOL_BACK HELP_04"),
+        InlineKeyboardButton("•ᴛᴏᴏʟs•", callback_data="TOOL_BACK HELP_10"),
     ],
     [
-        InlineKeyboardButton("• ᴠᴄ-ᴛᴏᴏʟs •", callback_data="TOOL_BACK HELP_14"),
-        InlineKeyboardButton("• ᴡʜɪsᴘᴇʀ •", callback_data="TOOL_BACK HELP_02"),
+        InlineKeyboardButton("•ᴠᴄ-ᴛᴏᴏʟs•", callback_data="TOOL_BACK HELP_14"),
+        InlineKeyboardButton("•ᴡʜɪsᴘᴇʀ•", callback_data="TOOL_BACK HELP_02"),
     ],
     [
         InlineKeyboardButton("⌯ ʙᴀᴄᴋ ᴛᴏ ʜᴏᴍᴇ ⌯", callback_data="MAIN_CP"),
